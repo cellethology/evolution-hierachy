7dd3ffdc6fd694dabe0a074f07a8bfb993420c9e
neonine2 <jerry.wang95@yahoo.ca>
2025-11-30 12:09:42 +0800

    .

--- plotting.py
+++ plotting.py
@@ -340,8 +340,8 @@ def plot_convergence_rate(
             color=color_map(idx),
             label=current_label,
         )
-    plt.xlabel("Total number of layers", fontsize=14)
-    plt.ylabel("Convergence rate \n (1/avg. gen. to optimality)", fontsize=14)
+    plt.xlabel("Total number of layers", fontsize=12)
+    plt.ylabel("Convergence rate \n (1/avg. gen. to optimality)", fontsize=12)
 
     # remove top and right spines, remove grid
     plt.gca().spines["top"].set_visible(False)
